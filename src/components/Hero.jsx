e19a0343781c07714594702f5d8566fc673c5ded
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-09-30 16:51:17 +0530

    Name Fix for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Hi, I am{" "}
+          <span className="h4 mb-6">
+            Hi, I am{" "}  </span>
+            <br></br>
             <span
   className="inline-block relative"
   style={{
