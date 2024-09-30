5df3b505bf9de19dbd681eaf0019361edd125c6d
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-09-30 16:54:19 +0530

    name fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25rem]"
+      className="pt-[6rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
